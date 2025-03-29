d87b45fef64171673b899d32b095521aaf1322be
aditya-waghmare1 <your-email@example.com>
2025-03-29 08:32:36 +0530

    Initial commit for Netflix Clone

---
+++ src/config.js
@@ -0,0 +1,20 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAthhGluWyZ3bDhlgH59hsUAo9gVD_p-kY",
+  authDomain: "chatapp-8edfc.firebaseapp.com",
+  projectId: "chatapp-8edfc",
+  storageBucket: "chatapp-8edfc.firebasestorage.app",
+  messagingSenderId: "204729076033",
+  appId: "1:204729076033:web:907715b3189cee327cf9c0",
+  measurementId: "G-06GXXHVYQ6"
+};
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGRmOGQ4NGQyMmMxZWExMzIyZGIxN2U0NWYwYzNmOCIsIm5iZiI6MTc0MzIxNTk4My42NDMwMDAxLCJzdWIiOiI2N2U3NWQ2ZmJkMWY5NmY5ZWY2MzY2OGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.lEeTeigJYI6Eh-hzwC6stL5qOEYVotJxIayd8QzrDN0"
+
+  export {firebaseConfig, TMDB_Access_Key};
